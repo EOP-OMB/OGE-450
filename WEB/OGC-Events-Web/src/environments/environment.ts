105c34383cbac5bd139aef9328d38af1219d8f55
Steven Kuennen <steven_b_kuennen@omb.eop.gov>
2020-12-15 16:07:50 -0500

    Updated environments.ts

--- WEB/OGC-Events-Web/src/environments/environment.ts
+++ WEB/OGC-Events-Web/src/environments/environment.ts
@@ -6,12 +6,12 @@
 export const environment = {
     production: false,
     envName: 'dev',
-    apiUrl: 'http://localhost:64217/api/',
-    clientId: 'd26b0eb6-6c0d-4292-8419-f3ea86a4f338',
+    apiUrl: 'Url to Web Api',
+    clientId: 'ADFS Client Id',
     base: '/',
     debug: true,
     idleTimeout: 840,
     idleCountdown: 60,
-    sharePointUrl: 'https://devportal.omb.gov/sites/ogc/',
-    portalUrl: 'https://accessdev.omb.gov/portal',
+    sharePointUrl: 'SharePoint Site Collection Url',
+    portalUrl: 'Url back to Portal (aka Training)',
 };
--- WEB/OGC-Training-Web/src/environments/environment.ts
+++ WEB/OGC-Training-Web/src/environments/environment.ts
@@ -6,13 +6,13 @@
 export const environment = {
     envName: 'dev',
     production: false,
-    apiUrl: 'http://localhost:50514/api/',
-    oge450Url: 'https://accessdev.omb.gov/ogc',
-    eventClearanceUrl: 'https://accessdev.omb.gov/events',
-    clientId: 'd26b0eb6-6c0d-4292-8419-f3ea86a4f338',
+    apiUrl: 'Url to Web API',
+    oge450Url: 'Url to OGE 450 App',
+    eventClearanceUrl: 'Url to Event Clearance App',
+    clientId: 'ADFS Client Id',
     base: '/',
     debug: true,
     idleTimeout: 840,
     idleCountdown: 60,
-    sharePointUrl: 'https://devportal.omb.gov/sites/ogc/',
+    sharePointUrl: 'Url to SharePoint',
 };
